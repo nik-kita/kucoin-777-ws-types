82c188712ecdc595d073876927ec001e3e6df69b
matmoimjs <matmoimjs@gmail.com>
2022-01-08 04:39:01 +0200

    fix TWsMarketTickerSub

--- src/pub-sub/market-ticker/market-ticker.sub.type.ts
+++ src/pub-sub/market-ticker/market-ticker.sub.type.ts
@@ -2,6 +2,6 @@ import { WsTopicEnum } from '../../enums/topic.enum';
 import { TWsSub } from '../../general/sub.type.general';
 
 export type TWsMarketTickerSub = TWsSub<{
-  topic: WsTopicEnum.MARKET_TICKER | WsTopicEnum.MARKET_TICKER_ALL,
+  topic: WsTopicEnum.MARKET_TICKER | WsTopicEnum.MARKET_TICKER_ALL | string,
   coins?: string[],
 }>
